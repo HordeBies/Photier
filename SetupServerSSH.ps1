0c18a11e0ddbdb9ef3dd929d685b3df770f07e49
Mehmet Demirci <73644073+HordeBies@users.noreply.github.com>
2023-12-13 16:34:49 +0300

    Update SetupServerSSH.ps1

--- SetupServerSSH.ps1
+++ SetupServerSSH.ps1
@@ -31,8 +31,8 @@ net start sshd
 # Install WinGet
 (New-Object System.Net.WebClient).DownloadFile("https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx", (Join-Path -Path (Get-Location) -ChildPath "Microsoft.VCLibs.x64.14.00.Desktop.appx"))
 (New-Object System.Net.WebClient).DownloadFile("https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx", (Join-Path -Path (Get-Location) -ChildPath "Microsoft.UI.Xaml.2.7.x64.appx"))
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/microsoft/winget-cli/releases/download/v1.7.3172-preview/b6e881d14bc943268a82d474bf7d15af_License1.xml", (Join-Path -Path (Get-Location) -ChildPath "b6e881d14bc943268a82d474bf7d15af_License1.xml"))
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/microsoft/winget-cli/releases/download/v1.7.3172-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle", (Join-Path -Path (Get-Location) -ChildPath "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"))
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/microsoft/winget-cli/releases/download/v1.6.3421/b6e881d14bc943268a82d474bf7d15af_License1.xml", (Join-Path -Path (Get-Location) -ChildPath "b6e881d14bc943268a82d474bf7d15af_License1.xml"))
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/microsoft/winget-cli/releases/download/v1.6.3421/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle", (Join-Path -Path (Get-Location) -ChildPath "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"))
 
 Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
